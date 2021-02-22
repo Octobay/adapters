3de79882c0cf663d9982d0ecea528972725da28d
mktcode <kontakt@markus-kottlaender.de>
2021-02-22 20:05:37 +0100

    added github user adapter

---
+++ githubUser.js
@@ -0,0 +1,66 @@
+const axios = require('axios')
+const axiosRetry = require('axios-retry')
+const { getEnvDefault } = require('./helper')
+const graphqlUrl = 'https://api.github.com/graphql'
+ 
+axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay })
+
+module.exports = (githubUser, accessToken = '') => {
+    accessToken = getEnvDefault(accessToken, 'GITHUB_PERSONAL_ACCESS_TOKEN')
+
+    return axios.post(graphqlUrl, {
+        query: `query($githubUser:String!) {
+            user(login: $githubUser) {
+                id
+                createdAt
+                updatedAt
+                login
+                url
+                name
+                location
+                avatarUrl
+                websiteUrl
+                twitterUsername
+                email
+                hasSponsorsListing
+                isHireable
+                isBountyHunter
+                isCampusExpert
+                isDeveloperProgramMember
+                followers {
+                    totalCount
+                }
+                organizations {
+                    nodes {
+                        id
+                        createdAt
+                        updatedAt
+                        login
+                        url
+                        name
+                        location
+                        avatarUrl
+                        websiteUrl
+                        twitterUsername
+                        email
+                        hasSponsorsListing
+                    }
+                }
+            }
+        }`,
+        variables: {
+            githubUser
+        }
+    }, {
+        headers: {
+            Authorization: 'bearer ' + accessToken
+        }
+    }).then(res => {
+        if (res.data.errors) {
+            throw res.data.errors
+        }
+        return res.data.data.user
+    }).catch(e => {
+        throw e
+    })
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     githubAccessToken: require('./githubAccessToken'),
+    githubUser: require('./githubUser'),
     bountyIsReleased: require('./bountyIsReleased'),
     addressRepoExists: require('./addressRepoExists')
 }
